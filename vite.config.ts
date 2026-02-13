183375c8195166900bca49a7e77394ffbe7ec36d
Ivo Pacheco Neto <101152222+IvoPachecoNeto@users.noreply.github.com>
2026-02-13 16:29:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,23 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
   return {
     plugins: [react()],
-    base: "./", // 🔥 MUITO IMPORTANTE
+    base: "./",
 
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
     }
   }
 })
-
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
